a4bf1bdde44cfd134d14468e81c5658fa5db4e42
ChrisBudow <christian.budow@columbia.edu>
2025-06-22 21:15:47 -0700

    shiny integration

--- Elevation_shp.R
+++ Elevation_shp.R
@@ -3,28 +3,49 @@ library(tidyverse)
 library(sf)
 library(leaflet)
 library(geosphere)
+library(shiny)
 
 getwd()
 setwd(getwd())
 
-shp <- read_sf(dsn = "import/Translink/bikeways/")
-df_shp <- shp %>%
-  select(object_id, street_name, segment_len, geometry)
+## FUNCTIon
 
-df_shp_t <- df_shp %>%
-  filter(object_id == "2581")
+# Read-in shapefile function
+Read_Shapefile <- function(shp_path) {
+  infiles <- shp_path$datapath # get the location of files
+  dir <- unique(dirname(infiles)) # get the directory
+  outfiles <- file.path(dir, shp_path$name) # create new path name
+  name <- strsplit(shp_path$name[1], "\\.")[[1]][1] # strip name 
+  purrr::walk2(infiles, outfiles, ~file.rename(.x, .y)) # rename files
+  x <- read_sf(file.path(dir, paste0(name, ".shp"))) # read-in shapefile
+  return(x)
+}
 
-df_shp_t$geometry <- st_sample(df_shp_t, size = 100)
-df_shp_t_p <- st_cast(df_shp_t, "POINT")
-
-crs_dd <- 4326
-df_h <- get_elev_point(df_shp_t_p, prj = crs_dd, src = "aws")
-
-df <- df_h %>%
-  mutate(
-    lon = st_coordinates(.)[,1],
-    lat = st_coordinates(.)[,2]
+# shp to df
+shp_to_df <- function(shp) {
+  df_shp <- shp %>%
+    select(object_id, street_name, segment_len, geometry)
+  
+  df_shp_t <- df_shp %>%
+    filter(object_id == "2581")
+  
+  df_shp_t$geometry <- st_sample(df_shp_t, size = 100)
+  df_shp_t_p <- st_cast(df_shp_t, "POINT")
+  
+  crs_dd <- 4326
+  df_h <- get_elev_point(df_shp_t_p, prj = crs_dd, src = "aws")
+  
+  df <- df_h %>%
+    mutate(
+      lon = st_coordinates(.)[,1],
+      lat = st_coordinates(.)[,2]
     )
+  return (df)
+}
+
+shp <- read_sf(dsn = "import/Translink/bikeways/")
+
+df1 <- shp_to_df(shp)
 
 up <- df %>%
   ggplot() +
@@ -39,27 +60,30 @@ down <- df %>%
         y = elevation)
   )
 
-point1 <- c(1, 2)
-point2 <- c(4, 6)
-a <- rbind(point1, point2)
-distance <- dist(a)
 
-dist(df$geometry)
+## SHINY
 
-leaflet(df_shp_t_p)
+ui <- fluidPage(
+  fileInput(
+    "filemap",
+    label = NULL,
+    multiple = TRUE,
+    accept = c('.shp','.dbf','.sbn','.sbx','.shx','.prj','.cpg')
+    ),
+  tableOutput("df"),
+  tableOutput("table")
+)
 
-plot(df_shp_t_p)
+server <- function(input, output, session) {
+  data <- reactive({
+    # Read-shapefile once user submits files
+    observeEvent(input$shp, {
+      user_shp <- Read_Shapefile(input$shp)
 
-leaflet(df_h) %>%
-  addTiles() %>%
-  addPolylines(df_h)
+    })
+    output$df <- renderTable(shp_to_df(user_shp))
+  })
+  output$table <- renderTable(df1)
+}
 
-
-
-b0 = st_polygon(list(rbind(c(-1,-1), c(1,-1), c(1,1), c(-1,1), c(-1,-1))))
-b1 = b0 + 2
-b2 = b0 + c(-0.2, 2)
-x = st_sfc(b0, b1, b2)
-st_area(x)
-line = st_sfc(st_linestring(rbind(c(30,30), c(40,40))), crs = 4326)
-st_length(df_shp_t)
+shinyApp(ui, server)
