ff431bf644c299303b160c7709cee4e56b1384fd
ChrisBudow <christian.budow@columbia.edu>
2025-06-25 21:04:22 -0700

    updated plots

--- Elevation_shp.R
+++ Elevation_shp.R
@@ -5,6 +5,7 @@ library(leaflet)
 library(geosphere)
 library(shiny)
 library(plotly)
+library(collapse)
 
 getwd()
 setwd(getwd())
@@ -28,14 +29,14 @@ shp_to_df <- function(shp) {
   df_shp <- shp %>%
     select(object_id, street_name, segment_len, geometry)
   
-  df_shp_t <- df_shp %>%
-    filter(object_id == "2581")
-  
-  df_shp_t$geometry <- st_sample(df_shp_t, size = 100)
-  df_shp_t_p <- st_cast(df_shp_t, "POINT")
+  df_shp$geometry <- st_sample(df_shp, size = 100)
+  df_shp_t_p <- st_cast(df_shp, "POINT")
   
   crs_dd <- 4326
   df_h <- get_elev_point(df_shp_t_p, prj = crs_dd, src = "aws")
+  
+  df_h <- df_h %>%
+    mutate(length = cumsum(segment_len))
   
   df <- df_h %>%
     mutate(
@@ -48,29 +49,54 @@ shp_to_df <- function(shp) {
 # tibble to df
 df_shp <- function(shpdf) {
   df <- shpdf %>%
-    st_drop_geometry() %>%
-    as.data.frame()
+    st_drop_geometry()
   return(df)
+}
+
+# leaflet map
+map_l <- function(t) {
+  map <- t %>%
+    leaflet() %>%
+    addTiles() %>%
+    addPolylines(
+      opacity = 1,
+      weight = 2.5
+    )
+  return(map)
 }
 
 #shp <- Read_Shapefile()
 shp <- read_sf(dsn = "import/Translink/bikeways/")
-df1 <- shp_to_df(shp)
+shp_t <- shp %>%
+  filter(object_id == "2581")
+df1 <- shp_to_df(shp_t)
 df <- df_shp(df1)
 
-up <- df %>%
-  ggplot() +
-  geom_sf(
-    aes(geometry = geometry)
-  )
+## Plots
 
-down <- df %>%
-  ggplot() +
-  geom_line(
-    aes(x = lon,
+t <- shp_t %>%
+  leaflet() %>%
+  addTiles() %>%
+  addPolylines(
+    opacity = 1,
+    weight = 2.5
+  )
+t <- map_l(shp_t)
+t
+
+l <- df %>%
+  ggplot(
+    aes(x = length,
         y = elevation)
+  ) +
+  geom_line() +
+  stat_smooth() +
+  theme_minimal() +
+  theme(
+    panel.grid.minor = element_blank()
   )
-
+l
+ggplotly(l)
 
 ## SHINY
 #https://stackoverflow.com/questions/71024105/why-wont-renderdt-return-a-table-on-my-shiny-app
